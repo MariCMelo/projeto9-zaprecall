b57c0fc304bc91b4589ea8905bfab18b5a73de05
Marilia <marilia.correa.melo@gmail.com>
2023-05-21 23:59:02 -0300

    finalizado corrigido bugs, primeira avaliação

--- src/components/Flashcards.jsx
+++ src/components/Flashcards.jsx
@@ -1,219 +1,187 @@
-import play from "../assets/seta_play.png"
-import redBtn from "../assets/icone_erro.png"
-import yellowBtn from "../assets/icone_quase.png"
-import greenBtn from "../assets/icone_certo.png"
-import turn from "../assets/seta_virar.png"
-import styled from "styled-components"
-import { useState } from "react"
+import React, { useState } from "react";
+import styled from "styled-components";
 
-export default function ({ card, index, setResult}) {
-    const [start, setStart] = useState(false)
-    const [turned, setTurned] = useState(false)
-    const [color, setColor] = useState("#333333")
-    const [addDash, setAddDash] = useState(false)
-    const [finish, setFinish] = useState(false)
-    const [btn, setBtn] = useState(play)
+import play from "../assets/seta_play.png";
+import redBtn from "../assets/icone_erro.png";
+import yellowBtn from "../assets/icone_quase.png";
+import greenBtn from "../assets/icone_certo.png";
+import turn from "../assets/seta_virar.png";
 
-  function openDeck() {
-    setStart(true);
-  }
-
-
+export default function Flashcard({ card, index, setResult }) {
+    const [start, setStart] = useState(false);
+    const [turned, setTurned] = useState(false);
+    const [color, setColor] = useState("#333333");
+    const [addDash, setAddDash] = useState(false);
+    const [btn, setBtn] = useState(play);
+    const [clickedList, setClickedList] = useState([]);
+    const [finish, setFinish] = useState(false);
 
     function openDeck() {
-        setStart(true)
+        if (!finish) {
+            setStart(true);
+        }
+    }
+
+    function isAnswered() {
+        if (!clickedList.includes(index)) {
+            setClickedList([...clickedList, index]);
+            setResult((prevResult) => prevResult + 1);
+        }
+    }
+
+    function handleButtonPress(btnColor, btnImage) {
+        setColor(btnColor);
+        setAddDash(true);
+        setStart(false);
+        setFinish(true);
+        setBtn(btnImage);
+        isAnswered();
     }
 
     function showAnswer() {
         if (!turned) {
             setTurned(true);
             setStart(true);
-          }
         }
-
-    function pressedRed() {
-        if (finish) return
-        setColor("#FF3030");
-        setAddDash(true);
-        setStart(false);
-        setBtn(redBtn);
-      }
-    
-      function pressedYellow() {
-        if (finish) return
-        setColor("#FF922E");
-        setAddDash(true);
-        setStart(false);
-        setBtn(yellowBtn); 
-      }
-    
-      function pressedGreen() {
-        if (finish) return
-        setColor("#2FBE34");
-        setAddDash(true);
-        setStart(false);
-        setBtn(greenBtn);
-      }
-
+    }
 
     return (
         <>
             {!start ? (
                 <CloseFlashcardContainer
-                 data-test="flashcard"
-                 color={color}
-                 addDash={addDash}
-                 >
+                    data-test="flashcard"
+                    color={color}
+                    addDash={addDash}>
                     <p data-test="flashcard-text">Pergunta {index + 1}</p>
-                    <img data-test="play-btn" onClick={openDeck} src={btn} alt="Icone de play" />
+                    <img
+                        data-test="play-btn"
+                        onClick={openDeck}
+                        src={btn}
+                        alt="Icone de play"
+                    />
                 </CloseFlashcardContainer>
-
             ) : (
-
                 <OpenFlashcardContainer>
-
                     {!turned ? (
-
                         <>
                             <p data-test="flashcard-text">{card.question}</p>
-                            <img data-test="turn-btn" onClick={showAnswer} src={turn} alt="Seta que vira o flashcard" />
+                            <img
+                                data-test="turn-btn"
+                                onClick={showAnswer}
+                                src={turn}
+                                alt="Seta que vira o flashcard"
+                            />
                         </>
-
                     ) : (
-
                         <>
                             <p data-test="flashcard-text">{card.answer}</p>
                             <ButtonsContainer>
-                            <RedButton 
-              className="RedButton"    
-              data-test = "no-btn"
-              onClick={pressedRed}
-              >Não lembrei</RedButton>
+                                <RedButton
+                                    onClick={() => handleButtonPress("#FF3030", redBtn)}
+                                >
+                                    Não lembrei
+                                </RedButton>
 
-              <YellowButton  
-              className="YellowButton"        
-              data-test = "partial-btn"    
-              onClick={pressedYellow}
-              >Quase não lembrei</YellowButton>
+                                <YellowButton
+                                    onClick={() => handleButtonPress("#FF922E", yellowBtn)}
+                                >
+                                    Quase não lembrei
+                                </YellowButton>
 
-              <GreenButton 
-              className="GreenButton"  
-              data-test = "zap-btn"          
-              onClick={pressedGreen}
-              >Zap!</GreenButton>
+                                <GreenButton
+                                    onClick={() => handleButtonPress("#2FBE34", greenBtn)}
+                                >
+                                    Zap!
+                                </GreenButton>
                             </ButtonsContainer>
                         </>
                     )}
                 </OpenFlashcardContainer>
             )}
         </>
-    )
+    );
 }
 
 const CloseFlashcardContainer = styled.div`
-    width: 300px;
-    height: 35px;
-    background-color: #FFFFFF;
-    margin: 12px;
-    padding: 15px;
-    box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
-    border-radius: 5px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-        p{
-            font-family: 'Recursive';
-            font-style: normal;
-            font-weight: 700;
-            font-size: 16px;
-            line-height: 19px;
-            color: ${props => props.color};
-            text-decoration: ${props => (props.addDash ? 'line-through' : 'none')};
-            
+  width: 300px;
+  height: 35px;
+  background-color: #ffffff;
+  margin: 12px;
+  padding: 15px;
+  box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-        }
-`
+  p {
+    font-family: "Recursive";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 19px;
+    color: ${(props) => props.color};
+    text-decoration: ${(props) => (props.addDash ? "line-through" : "none")};
+  }
+`;
+
 const OpenFlashcardContainer = styled.div`
-width: 300px;
-margin: 12px;
-padding: 15px;
-min-height: 100px;
-background: #FFFFD5;
-box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
-border-radius: 5px;
-font-family: 'Recursive';
-font-style: normal;
-font-weight: 400;
-font-size: 18px;
-line-height: 22px;
-color: #333333;
-position: relative;
-display: flex;
-flex-direction: column;
-justify-content: space-between;
-        img{
-            position: absolute;
-            bottom: 10px;
-            right: 10px;
-}
-`
+  width: 300px;
+  margin: 12px;
+  padding: 15px;
+  min-height: 100px;
+  background: #ffffd5;
+  box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+  border-radius: 5px;
+  font-family: "Recursive";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 22px;
+  color: #333333;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  img {
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+  }
+`;
 
 const ButtonsContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-    margin-top: 10px;
-`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 10px;
+`;
 
-const RedButton = styled.button`
-     width: 90px;
-        font-family: 'Recursive';
-        font-style: normal;
-        font-weight: 400;
-        font-size: 12px;
-        line-height: 14px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        text-align: center;
-        color: #FFFFFF;
-        background-color: #FF3030;
-        border-radius: 5px; 
-        padding:5px;
-        border: transparent;
-    ` 
+const Button = styled.button`
+  width: 90px;
+  font-family: "Recursive";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 14px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  color: #ffffff;
+  border-radius: 5px;
+  padding: 5px;
+  border: transparent;
+`;
 
-const YellowButton = styled.button`
-   width: 90px;
-        font-family: 'Recursive';
-        font-style: normal;
-        font-weight: 400;
-        font-size: 12px;
-        line-height: 14px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        text-align: center;
-        color: #FFFFFF;
-        background-color: #FF922E;
-        border-radius: 5px; 
-        padding:5px;
-        border: transparent;
-    ` 
+const RedButton = styled(Button)`
+  background-color: #ff3030;
+`;
 
-const GreenButton = styled.button`
-    width: 90px;
-        font-family: 'Recursive';
-        font-style: normal;
-        font-weight: 400;
-        font-size: 12px;
-        line-height: 14px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        text-align: center;
-        color: #FFFFFF;
-        background-color: #2FBE34;
-        border-radius: 5px; 
-        padding:5px;
-        border: transparent;
-    ` 
+const YellowButton = styled(Button)`
+  background-color: #ff922e;
+`;
+
+const GreenButton = styled(Button)`
+  background-color: #2fbe34;
+`;
--- src/components/GamePage.jsx
+++ src/components/GamePage.jsx
@@ -1,65 +1,62 @@
-import logo from "../assets/logo.png"
-import styled from "styled-components"
-import Flashcards from "./Flashcards"
-import Results from "./Results"
-import deck from "./Deck"
-import { useState } from "react"
+import React, { useState } from "react";
+import styled from "styled-components";
+import logo from "../assets/logo.png";
+import Flashcards from "./Flashcards";
+import Results from "./Results";
+import deck from "./Deck";
 
 export default function GamePage() {
-    const [result, setResult] = useState(0);
-    
-    return (
-        <GamePageContainer>
-            <HeaderContainer>
-                <img src={logo} alt="Logo ZapRecall" />
-                <h1>ZapRecall</h1>
-            </HeaderContainer>
+  const [result, setResult] = useState(0);
 
-            {deck.map((card, index) =>
-                <Flashcards
-                    key={card.question}
-                    card={card}
-                    index={index}
-                    setResult={setResult}
-                />
+  return (
+    <GamePageContainer>
+      <HeaderContainer>
+        <LogoImage src={logo} alt="Logo ZapRecall" />
+        <h1>ZapRecall</h1>
+      </HeaderContainer>
 
-            )}
+      {deck.map((card, index) => (
+        <Flashcards
+          key={card.question}
+          card={card}
+          index={index}
+          setResult={setResult}
+        />
+      ))}
 
-            <Results
-                totalCards={deck.length}
-                result={result}
-                
-            />
-        </GamePageContainer>
-    )
+      <Results totalCards={deck.length} result={result} />
+    </GamePageContainer>
+  );
 }
 
 const GamePageContainer = styled.div`
-    background-color: #FB6B6B;
-    width: 100vw;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin: 0px;
-    padding: 0px;
-    padding-bottom: 200px;
-    `
+  background-color: #fb6b6b;
+  width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+  padding-bottom: 200px;
+`;
 
 const HeaderContainer = styled.div`
-display: flex;
-align-items: center;
-margin: 40px 0 20px 0;
-    img {
-        width: 52px;   
-    }
-    h1{
-        font-family: 'Righteous';
-        font-style: normal;
-        font-weight: 400;
-        font-size: 36px;
-        line-height: 45px;
-        color: #FFFFFF;
-        margin-left: 20px;    
-    }
-`
+  display: flex;
+  align-items: center;
+  margin: 40px 0 20px 0;
+
+  h1 {
+    font-family: 'Righteous';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 36px;
+    line-height: 45px;
+    color: #ffffff;
+    margin-left: 20px;
+  }
+`;
+
+const LogoImage = styled.img`
+  width: 52px;
+`;
